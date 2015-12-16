1201f7cf6ab7ceed642a80ddc5c1fa026af83b26
Maekawa Yuichi <maekawa_yuichi@cyberagent.co.jp>
2015-12-16 15:59:24 +0900

    Add oval sample

--- sample/src/main/java/me/kaelaela/sample/MainActivity.java
+++ sample/src/main/java/me/kaelaela/sample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -27,5 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toolbar.setBackgroundColor(color);
         Drawable navigationIcon = DrawableColorChanger.changeColor(this, color, R.drawable.ic_arrow_back);
         toolbar.setNavigationIcon(navigationIcon);
+        ImageView oval = (ImageView) findViewById(R.id.oval);
+        color = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        Drawable newOval = DrawableColorChanger.changeColor(this, color, R.drawable.oval);
+        oval.setImageDrawable(newOval);
     }
 }
